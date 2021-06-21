3d8c0a4954d2baf78c89557c163dc8949dbac2fd
Nikhil Yadav <nickelyadav57@gmail.com>
2021-06-21 16:25:16 +0530

    active on click functionality added

---
+++ 11-tabs/app.js
@@ -0,0 +1,23 @@
+let btns = document.querySelectorAll('.tab-btn');
+let about = document.querySelector('.about');
+let articles = document.querySelectorAll('.content');
+
+about.addEventListener('click', function(e) {
+    const id = e.target.dataset.id;
+    if (id) {
+        // remove class active from all the buttons
+        btns.forEach(function(btn) {
+            btn.classList.remove('active');
+            // adding the class to the currently clicked button
+            e.target.classList.add('active');
+        })
+
+        //hide other articles
+        articles.forEach(function(article){
+            article.classList.remove('active');
+        });
+
+        const element = document.getElementById(id);
+        element.classList.add('active');
+    }
+});
